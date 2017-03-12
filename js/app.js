3cb8a83b6430b15268b976e3fe66dce39a5ae939
trentonlawton <trentonlawton@gmail.com>
2017-03-11 22:35:20 -0800

    B-1000 sessionStorage working and styling for desktop almost done

--- js/app.js
+++ js/app.js
@@ -1,86 +1,112 @@
-  var newArray= [];
+
+var newArray = [];
 var app = new Vue({
-el:'#app',
-data:{
-  counter:0,
-  message:'hello world',
-  imgSrc:'default',
-  title:'hello'
-}
+  el: '#app',
+  data: {
+    counter: 0,
+    message: 'hello world',
+    imgSrc: 'default',
+    title: 'hello'
+  }
 })
 
 /*this function should return necessary image sizing based on viewport size */
-function imageSize(windowSize){
-  if(windowSize <750 ){
+function imageSize(windowSize) {
+  if (windowSize < 750) {
     console.log("phone");
     console.log(windowSize);
     return "medium_large";
-  }
-  else if(windowSize >750 &&  windowSize <1024 ){
+  } else if (windowSize > 750 && windowSize < 1024) {
     console.log("tablet");
     console.log(windowSize);
     return "medium_large";
-  }
-  else{
+  } else {
     console.log("desktop time")
     return "large";
-    }
   }
-
-
-  // TODO: onload needs to grab json file and save it to session sessionStorage
-  // TODO: if the page alsready has data in sesssion storage check that its upto date
-  // TODO: if not then it should make another json request
-  /*this function should grab scaled data based on current viewport */
-function loader(){
-  var hello = setTimeout(showGallery,2000)
 }
 
-function showGallery(){
+
+// TODO: onload needs to grab json file and save it to session sessionStorage
+// TODO: if the page alsready has data in sesssion storage check that its upto date
+// TODO: if not then it should make another json request
+/*this function should grab scaled data based on current viewport */
+
+function initialize() {
+
+  $.getJSON("http://www.usernameisnull.com/data/wp-json/wp/v2/posts/?_embed", function(data) {
+    var newdata = JSON.stringify(data)
+    sessionStorage.setItem('data', newdata)
+    console.log(JSON.parse(sessionStorage.data))
+  })
+}
+function loader() {
+  var hello = setTimeout(showGallery, 2000)
+}
+
+function showGallery() {
   $('.container').fadeIn(2000);
-  $('.loader').remove();
+  $('.loaderContainer').remove();
 }
 
-  function getData(){
-
-    var screenSize = imageSize($(window).width());
-      $.getJSON("http://www.usernameisnull.com/data/wp-json/wp/v2/posts/?_embed",function(data){
-console.log(data[app.counter]);
-localStorage.setItem('test',data)
-app.imgSrc = data[app.counter]._embedded['wp:featuredmedia'][0].media_details.sizes[screenSize].source_url;
-app.title = data[app.counter].title.rendered;
-
-
+// NOTE: This function renders appropriate assets based on glass size the idea is that we don't load full sized images when we're on our phones
+function render() {
+  $('#about').hide();
+  var screenSize = imageSize($(window).width());
+  var data = JSON.parse(sessionStorage.data)
+  console.log(data[app.counter]);
+  localStorage.setItem('test', data)
+  app.imgSrc = data[app.counter]._embedded['wp:featuredmedia'][0].media_details.sizes[screenSize].source_url;
+  app.title = data[app.counter].title.rendered;
   var newdata = JSON.stringify(data)
-  app.message =data[app.counter].content.rendered;
-  sessionStorage.setItem('data',newdata)
-      console.log(JSON.parse(sessionStorage.data))
-      })
-
-  }
-  loader();
-  getData()
+  app.message = data[app.counter].content.rendered;
+}
+loader();
+render()
 
 // TODO: update to use sessionStorage that way there is only one initial payload from wp api
 // TODO: restructure function to rely on sessionStorage array
-  function browse(){
-  $('#prev').click(function(){
-    if(app.counter != 0){
+function browse() {
+  $('#prev').click(function() {
+    if (app.counter != 0) {
       app.counter--;
-      getData()
-    }
-    else{
+      render()
+    } else {
       console.log('at the beginning');
     }
   })
-  $('#next').click(function(){
-    if(app.counter)
-    app.counter++;
-    getData();
+  $('#next').click(function() {
+    if (app.counter < JSON.parse(sessionStorage.data).length - 1) {
+      app.counter = app.counter + 1;
+      console.log(app.counter);
+      render();
+    } else {
+      console.log("Woah there cowboy theres no more images");
+    }
   })
-  }
+}
 
 
-    browse()
+browse()
+function navigation() {
+  $('#aboutButton').click(function() {
+    $('#gallery').fadeOut(2000);
+    $('#movement').fadeOut(2000);
+    setTimeout(() => {
+      $('#about').show()
+      app.message = "<p>Trenton Lawton is a San Francisco based Designer,developer,and visual artist.Lo-fi fam salvia disrupt, typewriter fanny pack chia biodiesel twee artisan selvage edison bulb quinoa bushwick. Seitan beard paleo live-edge. Next level actually viral, tote bag pitchfork cray tousled DIY +1 lo-fi chartreuse neutra crucifix green juice narwhal. Fingerstache cronut keffiyeh beard whatever, dreamcatcher art party taxidermy meh organic. Sartorial sriracha literally unicorn. Etsy ramps green juice whatever, offal XOXO asymmetrical poke letterpress neutra four loko +1 glossier coloring book.Everyday carry swag iPhone affogato williamsburg celiac.</p><div class='logoContainer'><img src='assets/newlogo.png' class='logos'><img src='assets/skulllogo.png' class='logos'></div>"
+    }, 2000);
+  })
 
-    console.log('hello');
+  $('#homeButton').click(function() {
+    $('#about').fadeOut(2000);
+    setTimeout(() => {
+      render();
+      $('#gallery').show()
+      $('#movement').show();
+    }, 2000);
+  })
+}
+
+navigation();
+console.log('hello');
